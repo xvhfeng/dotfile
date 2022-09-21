ff1e302113e7a9628fa4627441548275ad63c482
大嘴 <xvhfeng@gmail.com>
2022-09-21 19:16:24 +0800

    new keymapping for folding

--- nvim/lua/core/default.lua
+++ nvim/lua/core/default.lua
@@ -203,7 +203,7 @@ default_setting['opt'] = {
      --  " syntax    使用语法定义折叠
      --  " diff      对没有更改的文本进行折叠
      --  " marker    使用标记进行折叠, 默认标记是 {{{ 和 }}}
-     foldmethod="manual",
+     foldmethod="marker",
      foldlevel=1,
  
      -- " 缩进配置
--- nvim/lua/core/keymapping.lua
+++ nvim/lua/core/keymapping.lua
@@ -597,12 +597,26 @@ global_mapping.register({
     short_desc = "Split window and open term"
 })
 
---global_mapping.register({
---    mode = "n",
---    key = { "z", "f" },
---    action = '<ESC>zf%',
---    short_desc = "folding"
---})
+--[===[
+zc	关闭当前打开的折叠
+zo	打开当前的折叠
+zm	关闭所有折叠
+zM	关闭所有折叠及其嵌套的折叠
+zr	打开所有折叠
+zR	打开所有折叠及其嵌套的折叠
+zd	删除当前折叠
+zE	删除所有折叠
+zj	移动至下一个折叠
+zk	移动至上一个折叠
+zn	禁用折叠
+zN	启用折叠
+--]===]
+global_mapping.register({
+    mode = "n",
+    key = { "z", "f" },
+    action = 'zf%',
+    short_desc = "folding"
+})
 
 
 -- buffer configure at bufferline plugin
