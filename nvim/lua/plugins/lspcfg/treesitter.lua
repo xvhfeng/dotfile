a179b9db60fc558d2cb9af7dfa986ca3ab083a51
大嘴 <xvhfeng@gmail.com>
2023-02-01 23:39:47 +0800

    highlight by tree-sitter is success

--- nvim/lua/plugins/lspcfg/treesitter.lua
+++ nvim/lua/plugins/lspcfg/treesitter.lua
@@ -4,7 +4,7 @@ plugin.core = {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
     require('nvim-treesitter.configs').setup {
-        ensure_installed = { "vim","help","bash","c","cpp","lua" },
+        ensure_installed = { "vim","help","bash","c","cpp","lua","java","go"},
         highlight = {
             enable = true, -- false will disable the whole extension
             additional_vim_regex_highlighting = false,
