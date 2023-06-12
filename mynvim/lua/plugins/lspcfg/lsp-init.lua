fac6c0aba6421d73cbb6bf545915da636b73d317
大嘴 <xvhfeng@gmail.com>
2023-06-12 15:01:39 +0800

    add lspconfig keymapping

--- mynvim/lua/plugins/lspcfg/lsp-init.lua
+++ mynvim/lua/plugins/lspcfg/lsp-init.lua
@@ -43,6 +43,38 @@ function loading()
 
     local lspconfig = require('lspconfig')
 
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+    -- Use an on_attach function to only map the following keys
+    -- after the language server attaches to the current buffer
+    local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+    end
+
+
     require("mason-lspconfig").setup_handlers({
         function(server_name)
             require("lspconfig")[server_name].setup {}
@@ -58,14 +90,16 @@ function loading()
                     }
                 }
             }
+            on_attach = on_attach
         end,
         ["clangd"] = function()
             lspconfig.clangd.setup {
                 cmd = {"clangd", -- "--header-insertion=never",
-                "--query-driver=/opt/homebrew/opt/llvm/bin/clang++" -- "--all-scopes-completion",
-                -- "--completion-style=detailed",
+                    "--query-driver=/opt/homebrew/opt/llvm/bin/clang++" -- "--all-scopes-completion",
+                    -- "--completion-style=detailed",
                 }
             }
+            on_attach = on_attach
         end
     })
 
@@ -79,10 +113,10 @@ plugin.core = {
         cmd = "Neoconf",
         config = true
     }, {
-        "folke/neodev.nvim",
-        opts = {}
-    }, {"williamboman/mason.nvim" ,
-build = "MasonUpdate"}, {"williamboman/mason-lspconfig.nvim"}, {"hrsh7th/cmp-nvim-lsp"}},
+            "folke/neodev.nvim",
+            opts = {}
+        }, {"williamboman/mason.nvim" ,
+            build = "MasonUpdate"}, {"williamboman/mason-lspconfig.nvim"}, {"hrsh7th/cmp-nvim-lsp"}},
 
     init = function()
         loading()
