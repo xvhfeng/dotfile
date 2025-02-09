0f6d6039e29193fae7708c1aa4288e0b82c0a37c
bigmouth <xvhfeng@gmail.com>
2025-02-09 18:26:23 +0800

    change the floder

--- mynvim/lua/core/plugins.lua
+++ mynvim/lua/core/plugins.lua
@@ -10,7 +10,7 @@ local tbldump = require("util.tbldump")
 
 xlog.trace("load plugins.lua")
 
-plugins_configure.plugins_groups = {
+plugins_configure.plugins_groups = { -- {{{
 	[1] = {
 		["name"] = "default",
 		["subpath"] = "",
@@ -24,60 +24,20 @@ plugins_configure.plugins_groups = {
 			{ name = "dressing", enable = true, desc = "更好看的输入框" },
 			{ name = "project", enable = true, desc = "项目管理" },
 			{ name = "vimdoc", enable = true, desc = "中文的vimdoc帮助" },
-			{
-				name = "auto_mkdir",
-				enable = true,
-				desc = "save的时候自动创建目录",
-			},
-			{
-				name = "autojump",
-				enable = true,
-				desc = "集成autojump工具在vim中",
-			},
+			{ name = "auto_mkdir", enable = true, desc = "save的时候自动创建目录" },
+			{ name = "autojump", enable = true, desc = "集成autojump工具在vim中" },
 			{ name = "FencView", enable = true, desc = "多字节支持" },
-			{
-				name = "genutils",
-				enable = true,
-				desc = "vim的工具函数集合",
-			},
-			{
-				name = "indent-blankline",
-				enable = true,
-				desc = "展现缩进的插件,但是只能在nvim中使用,不支持vim,可以换成支持vim的那个",
-			},
+			{ name = "genutils", enable = true, desc = "vim的工具函数集合" },
+			{ name = "indent-blankline", enable = true, desc = "展现缩进插件,只在nvim中使用" },
 			{ name = "rainbow", enable = true, desc = "括号颜色匹配" },
 			-- {name = "autosession", enable = false, desc = "session保存"},
 			{ name = "vim-airline", enable = true, desc = "vim的状态栏" },
-			{
-				name = "vim-emacscommandline",
-				enable = true,
-				desc = "vim的命令中绑定emacs按键模式",
-			},
-			{
-				name = "vim-highlight-cursor-words",
-				enable = true,
-				desc = "高亮当前光标下的word",
-			},
-			{
-				name = "vim-repeat",
-				enable = true,
-				desc = "vim的重复命令插件",
-			},
-			{
-				name = "asynctasks",
-				enable = true,
-				desc = "异步执行工具,依赖于skywind3000/asyncrun.vim",
-			},
-			{
-				name = "remember",
-				enable = true,
-				desc = "打开文件的时候,回到上一次编辑的地方",
-			},
-			{
-				name = "toggleterm",
-				enable = true,
-				desc = "虚拟终端，执行系统bash与lazydocker",
-			},
+			{ name = "vim-emacscommandline", enable = true, desc = "vim的命令中绑定emacs按键模式" },
+			{ name = "vim-highlight-cursor-words", enable = true, desc = "高亮当前光标下的word" },
+			{ name = "vim-repeat", enable = true, desc = "vim的重复命令插件" },
+			{ name = "asynctasks", enable = true, desc = "异步执行工具,依赖于skywind3000/asyncrun.vim" },
+			{ name = "remember", enable = true, desc = "打开文件的时候,回到上一次编辑的地方" },
+			{ name = "toggleterm", enable = true, desc = "虚拟终端，执行系统bash与lazydocker" },
 			--[[
             {
                 name = "noice",
@@ -91,17 +51,10 @@ plugins_configure.plugins_groups = {
 			--    enable = true,
 			--    desc = "可以搜索的keymap function command绑定插件",
 			--},
-			{
-				name = "vim-fakeclip",
-				enable = true,
-				desc = "vim与系统之间共享clip",
-			},
+			{ name = "vim-fakeclip", enable = true, desc = "vim与系统之间共享clip" },
 			-- { name = "wilder", enable = false, desc = "更好的命令行功能"},
-			{
-				name = "which-key",
-				enable = true,
-				desc = "vim的操作快捷键映射",
-			}, --  { name="test", enable = true, desc="测试插件"},
+			{ name = "which-key", enable = true, desc = "vim的操作快捷键映射" },
+			--  { name="test", enable = true, desc="测试插件"},
 		},
 	},
 	[2] = {
@@ -125,44 +78,20 @@ plugins_configure.plugins_groups = {
 		["plugins"] = {
 			{ name = "nvimtree", enable = true, desc = "lua写的floder" },
 			-- {name = "neo-tree", enable = true, desc = "lua写的floder"},
-			{
-				name = "bufexplorer",
-				enable = true,
-				desc = "buffer的资源管理器",
-			},
+			{ name = "bufexplorer", enable = true, desc = "buffer的资源管理器" },
 			--{
 			--    name = "maximizer",
 			--    enable = true,
 			--    desc = "最大化window,并且恢复原来打开的window状态",
 			--},
 			-- { name = "ranger", enable = true, desc = "floder管理器" },
-			{
-				name = "rename",
-				enable = true,
-				desc = "重名当前打开的文件",
-			},
-			{
-				name = "qf-helper",
-				enable = true,
-				desc = "一个更好的quickfix的替代品",
-			},
+			{ name = "rename", enable = true, desc = "重名当前打开的文件" },
+			{ name = "qf-helper", enable = true, desc = "一个更好的quickfix的替代品" },
 			{ name = "kickfix", enable = true, desc = "filter for quickfix" },
-			{
-				name = "vimade",
-				enable = true,
-				desc = "高亮显示当前foced分割窗口",
-			},
-			{
-				name = "windowpicker",
-				enable = true,
-				desc = "vim分屏的时候,快速选择window",
-			},
+			{ name = "vimade", enable = true, desc = "高亮显示当前foced分割窗口" },
+			{ name = "windowpicker", enable = true, desc = "vim分屏的时候,快速选择window" },
 			{ name = "winresizer", enable = true, desc = "调整window的大小" },
-			{
-				name = "window",
-				enable = true,
-				desc = "只是对于windows的key mapping的操作",
-			},
+			{ name = "window", enable = true, desc = "只是对于windows的key mapping的操作" },
 			{ name = "neoscroll", enable = true, desc = "光标不移动，底下的文字移动" },
 			{ name = "close-buffers", enable = true, desc = "快速关闭其它/隐藏/当前等buffer" },
 			-- {name = "oil", enable = true, desc = "buffer模式管理文件夹"},
@@ -172,16 +101,8 @@ plugins_configure.plugins_groups = {
 		["name"] = "finder",
 		["subpath"] = "finder",
 		["plugins"] = {
-			{
-				name = "telescope",
-				enable = true,
-				desc = "高度可扩展的列表模糊查找工具",
-			},
-			{
-				name = "grug-far",
-				enable = true,
-				desc = "一个使用rg 或 ast-grep进行查找或者是替换的插件。",
-			},
+			{ name = "telescope", enable = true, desc = "高度可扩展的列表模糊查找工具" },
+			{ name = "grug-far", enable = true, desc = "神器 查找或者是替换的插件。" },
 			{ name = "fzf", enable = true, desc = "查找操作" },
 			-- { name = "narrow", enable = true, desc = "自动删除行尾空格" },
 			--[[ {name = "spectre", enable = true, desc = "搜索与替换插件"},
@@ -210,41 +131,15 @@ plugins_configure.plugins_groups = {
 		["name"] = "sltdo",
 		["subpath"] = "sltdo",
 		["plugins"] = {
-			{
-				name = "nvim-hlslens",
-				enable = true,
-				desc = "高亮显示匹配信息,并且可以在匹配之间跳转,和asteris,matchup一起配合",
-			},
+			{ name = "nvim-hlslens", enable = true, desc = "高亮匹配且跳转" },
 			{ name = "hop", enable = true, desc = "快速移动插件" },
 			{ name = "vim-bookmarks", enable = true, desc = "标记插件" },
 			-- { name = "marks", enable = false, desc = "标记插件" },
-			{
-				name = "vim-unimpaired",
-				enable = true,
-				desc = "[ ] 开头的快捷键,vim缺失的快捷键",
-			},
-			{
-				name = "vim-asterisk",
-				enable = true,
-				desc = "不移动鼠标通过*/#选择文本",
-				-- desc = "一个显示诊断、参考、telescope结果、快速修复和位置列表的漂亮列表，可以帮助您解决代码引起的所有问题。"
-			},
-			{
-				name = "vim-matchup",
-				enable = true,
-				desc = "使用%查找对应的范围标签",
-			},
-			{
-				name = "vim-visual-multi",
-				enable = true,
-				desc = "多光标插件,可同时编辑选中的多出统一word",
-			},
-
-			{
-				name = "sltdo",
-				enable = true,
-				desc = "navigation的key-mapping。",
-			},
+			{ name = "vim-unimpaired", enable = true, desc = "[ ] 开头的快捷键,vim缺失的快捷键" },
+			{ name = "vim-asterisk", enable = true, desc = "不移动鼠标通过*/#选择文本" },
+			{ name = "vim-matchup", enable = true, desc = "使用%查找对应的范围标签" },
+			{ name = "vim-visual-multi", enable = true, desc = "多光标插件" },
+			{ name = "sltdo", enable = true, desc = "navigation的key-mapping。" },
 		},
 	},
 	[6] = {
@@ -363,7 +258,9 @@ plugins_configure.plugins_groups = {
 			{ name = "folding-hooks", enable = true, desc = "Code Folding hooks,依赖nvim-ufo" },
 		},
 	},
-}
+	-- region
+} -- }}}
+-- endregion
 
 plugins_configure.setup = function()
 	xlog.trace("myplugins's setup called")
@@ -554,7 +451,9 @@ plugins_configure.setup = function()
 	for idx, hk in ipairs(hooks) do
 		hk()
 	end
+	-- region
 end
+-- endregion
 
 plugins_configure.create_mapping = function()
 	xlog.trace("myplugins's create_mapping called")
