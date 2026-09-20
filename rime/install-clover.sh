c3d3654b073ed4ed047d4c756496e8931fee6faf
xvhfeng <xuhaifeng@canfengtech.com>
2026-09-20 18:16:58 +0800

    增加安装脚本

---
+++ rime/install-clover.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : xvhfeng (xvhfeng@xvhfengdeMacBook-Air.local)
+# @file        : install-clover
+# @created     : Sunday Sep 20, 2026 18:11:28 CST
+#
+# @description :
+######################################################################
+
+[ -f $HOME/Library/Rime ] && rm -rf $HOME/Library/Rime
+
+ln -s $(pwd)/clover $HOME/Library/Rime
+
+
+
